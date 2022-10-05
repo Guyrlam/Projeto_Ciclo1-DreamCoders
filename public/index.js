158bbdea114792d446150b1786a56da0362983a6
antonio francisco <antoniofco20142014@gmail.com>
2022-10-05 11:09:41 -0300

    modal contato

--- public/index.js
+++ public/index.js
@@ -19,6 +19,32 @@ const main = document.querySelector('#root')
             
         }
     })
+    const switchModal = () => {
+        const modal = document.querySelector('.modal')
+        const actualStyle = modal.style.display
+        if(actualStyle == 'block') {
+          modal.style.display = 'none'
+        }
+        else {
+          modal.style.display = 'block'
+        }
+      }
+      
+      const btn = document.querySelector('.modalBtn')
+      btn.addEventListener('click', switchModal)
+      
+      window.onclick = function(event) {
+          const modal = document.querySelector('.modal')
+        if (event.target == modal) {
+          switchModal()
+        }
+        
+    
+      }
+      document.querySelector('.fechar').onclick = function(event) {
+       
+        switchModal()
+    }
 
 
 
