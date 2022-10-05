341004627c8e78956797ccf564ca324eab344b2b
Daniel.dr206@gmail.com <Daniel.dr206@gmail.com>
2022-10-05 13:10:57 -0300

    feat: adicionando pagina de detalhes dos livros

--- public/index.js
+++ public/index.js
@@ -5,9 +5,9 @@ import login from "./pages/login/index.js"
 const main = document.querySelector('#root')
 
 
-// window.addEventListener("load", () => {
-//     main.appendChild(home())
-// })
+ window.addEventListener("load", () => {
+     main.appendChild(home())
+ })
 
 window.addEventListener("hashchange", () => {
     main.innerHTML = ""
