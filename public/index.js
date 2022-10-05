b20c67487b19ef696d5d8393c2c21d11cbc73381
Daniel.dr206@gmail.com <Daniel.dr206@gmail.com>
2022-10-05 17:37:27 -0300

    fix: conflitos

--- public/index.js
+++ public/index.js
@@ -4,13 +4,12 @@ import home from "./pages/home/index.js"
 import login from "./pages/login/index.js"
 import profile from "./pages/profile/index.js"
 import signup from "./pages/signup/index.js"
-const main = document.querySelector('#root')
 
 const main = document.querySelector('#root');
 
-//  window.addEventListener("load", () => {
-//      main.appendChild(home())
-//  })
+//   window.addEventListener("load", () => {
+//       main.appendChild(home())
+//   })
 
 window.addEventListener("hashchange", () => {
     main.innerHTML = ""
