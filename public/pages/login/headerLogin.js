9d9a392a9bc94655671d2fa84694d22251fdd8a1
Daniel.dr206@gmail.com <Daniel.dr206@gmail.com>
2022-10-07 12:50:40 -0300

    feat: add book to database

--- public/pages/login/headerLogin.js
+++ public/pages/login/headerLogin.js
@@ -1,11 +1,13 @@
 export default async function headerLogin(){
     const container = document.createElement('div');
     const template = `
+    <a href="#profile">
     <span id="hello-user">
                         <img src="./assets/icon-user.png" alt="" id="icon-user" width="27px" height="27px">
                         Olá Usuário
                         
     </span>
+    </a>
     `
     container.innerHTML = template
     return container
