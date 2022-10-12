5c2ee4e8d070ad8e09bd8aac94d0c26f1f3637c5
Ronaldo Sc <rs.quim@gmail.com>
2022-10-12 20:48:18 -0300

    feat: search implementation - the saga

--- public/pages/home/search/search.js
+++ public/pages/home/search/search.js
@@ -1,67 +1,31 @@
-import { temp } from "../../books";
+const userCardTemplate = document.querySelector("[data-user-template]")
+const userCardContainer = document.querySelector("[data-user-cards-container]")
+const searchInput = document.querySelector("[data-search]")
 
-const searchTerms = [];
-const word = document.getElementById("header-search-bar");
+let bookResults = []
 
-function insertWord() {
-    const filterWords = word.value
-    filterWords.replace(/ /g, ',')
-    filterWords.split(' ')
-    searchTerms.push();
-    // console.log(palavras);
-    // let wordsAdded = document.getElementById("allWords");
+searchInput.addEventListener("input", e => {
+    const word = e.target.value.toLowerCase()
+    bookResults.forEach(book => {
+        const isVisible =
+            book.name.toLowerCase().includes(word) ||
+            book.email.toLowerCase().includes(word)
+        book.element.classList.toggle("hide", !isVisible)
+    })
+})
 
+fetch(`//localhost:8080/`)
+    .then(res => res.json())
+    .then(data => {
+        books = data.map(book => {
+            const card = userCardTemplate.content.cloneNode(true).children[0]
+            const header = card.querySelector("[data-header]")
+            const body = card.querySelector("[data-body]")
+            header.textContent = book.name
+            body.textContent = book.email
+            userCardContainer.append(card)
+            return { name: book.name, email: book.email, element: card }
+        })
+    })
 
-
-    wordsAdded.innerHTML = "";
-    for (let i = 0; i < searchTerms.length; i++) {
-        wordsAdded.innerHTML += `<p>${searchTerms[i]}</p>`;
-    }
-    return;
-}
-temp()
-function exhibit() {
-    let result = document.getElementById("result");
-    result.innerHTML = "";
-    for (let i = 0; i < searchTerms.length; i++) {
-        if (word.value === searchTerms[i]) {
-            result.innerHTML += `<p>${searchTerms[i]}</p>`;
-        }
-    }
-}
-
-class adenter {
-    /**
-        * Gets the list of search engines
-        *
-        * @returns List of engines
-        * @type object
-        */
-
-    get engines() {
-        var engines = [];
-        var tree = this.getElement({ type: "engine_list" }).getNode();
-        for (var ii = 0; ii < tree.view.rowCount; ii++) {
-            engines.push({
-                name: tree.view.getCellText(ii, tree.columns.getColumnAt(0)),
-                keyword: tree.view.getCellText(ii, tree.columns.getColumnAt(1))
-            });
-        }
-        return engines;
-    },
-    /**
- * Gets the name of the selected search engine
- *
- * @returns Name of the selected search engine
- * @type string
- */
-    get selectedEngine() {
-        var treeNode = this.getElement({ type: "engine_list" }).getNode();
-        if (this.selectedIndex != -1) {
-            return treeNode.view.getCellText(this.selectedIndex,
-                treeNode.columns.getColumnAt(0));
-        } else {
-            return null;
-        }
-    }
-}
+export default search
