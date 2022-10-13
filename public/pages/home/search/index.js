af88fd3c61b09dfc76adfcc416a280a5122bc7f9
Daniel.dr206@gmail.com <Daniel.dr206@gmail.com>
2022-10-13 10:50:17 -0300

    feat: search

---
+++ public/pages/home/search/index.js
@@ -0,0 +1,15 @@
+export default async (value) => {
+    const req = await fetch('//localhost:8080/book');
+    const result = await req.json();
+    console.log(value)
+    const list = [];
+    for (let i = 0; i < result.data.length; i++) {
+        const el = result.data[i];
+        if (el.name.toLowerCase() == value.toLowerCase() ||
+        el.publisher.toLowerCase() == value.toLowerCase()
+        ) {
+            list.push(el);
+        }
+    }
+    return list;
+}
---
+++ public/pages/home/search/showBooksSearch.js
@@ -0,0 +1,39 @@
+
+import search from "./index.js";
+const pesquisa = document.querySelector('#header-search-bar')
+const bookSearch = await search(pesquisa.value)
+//import details from '../details/index.js'
+
+
+function list(data) {
+    let ktem = '<section id="books">'
+        ;
+    for (var i = 0; i < data.length; i++) {
+        ktem +=
+            `
+            <div class="book-feed">
+                <a class="a-book-feed">
+                    <img src="${data[i].images[0]}" alt="" class="img-book-feed" id="${data[i].id}">
+                </a>
+                <p>${data[i].name}</p>
+            </div>
+            `;
+    }
+    return ktem += '</section>';
+}
+
+export default async function feed() {
+    const container = document.createElement('div');
+
+
+    search(pesquisa.value).then((data) => {
+        container.innerHTML = list(data);
+    })
+    //container.innerHTML = template;
+
+    return container;
+};
+
+
+
+
